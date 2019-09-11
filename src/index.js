42f374d271a8cc61ed72d8d6b290fe4f5a4e0398
Syed Shameer Sarwar <syedshameersarwar@gmail.com>
2019-09-11 12:35:14 +0500

    Adding HashRouter

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<BrowserRouter> <App /> </BrowserRouter>,
+ReactDOM.render(<HashRouter> <App /> </HashRouter>,
          document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
