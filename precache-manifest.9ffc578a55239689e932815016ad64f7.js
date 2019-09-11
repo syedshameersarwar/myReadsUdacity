bb48b0f996e3ebe81bad2d39ab265257a7834022
Syed Shameer Sarwar <syedshameersarwar@gmail.com>
2019-09-11 12:09:06 +0500

    Updates

---
+++ precache-manifest.9ffc578a55239689e932815016ad64f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcaa9747fd6fe440c5b557b90cddd614",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a05f393884389c133bab",
+    "url": "/static/css/main.01e17f7d.chunk.css"
+  },
+  {
+    "revision": "fcb418b6d743accfcf77",
+    "url": "/static/js/2.b19cbefd.chunk.js"
+  },
+  {
+    "revision": "a05f393884389c133bab",
+    "url": "/static/js/main.7171ebab.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/static/media/arrow-drop-down.78c6f53b.svg"
+  }
+]);
